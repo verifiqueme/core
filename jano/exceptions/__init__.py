30a1dcd9fc3b009e65c214d2cdeda52a014eeb3f
Guilherme Scaranse <guiscaranse@gmail.com>
2018-12-27 15:08:12 -0300

    Adicionado exceções

---
+++ jano/exceptions/__init__.py
@@ -0,0 +1,10 @@
+class JunoException(BrokenPipeError):
+    def __init__(self, message, *args):
+        self.message = message
+        super(JunoException, self).__init__(message, *args)
+
+
+class NoAzureKey(JunoException):
+    def __init__(self, message, *args):
+        self.message = message
+        super(NoAzureKey, self).__init__(message, *args)
