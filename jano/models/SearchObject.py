cf9a30d6723f476c8cd8cf80c6064526315eb2fa
Guilherme Scaranse <guiscaranse@gmail.com>
2018-12-27 15:39:41 -0300

    Ajustado para todos os elementos data serem padrozinados

--- jano/models/SearchObject.py
+++ jano/models/SearchObject.py
@@ -1,7 +1,11 @@
+import pendulum
+
+from jano.config import Config
+
+
 class SearchObject(object):
     def __init__(self, titulo, url, descricao, data):
         self.titulo = titulo
         self.url = url
         self.descricao = descricao
-        self.data = data
-
+        self.data = pendulum.parse(data, tz=Config().values()['timezone'], strict=False)
