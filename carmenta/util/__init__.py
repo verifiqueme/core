ad8fa672878a11869924cf6bc467f66338261d59
Guilherme Scaranse <guiscaranse@gmail.com>
2018-12-29 12:56:41 -0300

    Adicionado NLTK e ferramenta de remover stopwords

---
+++ carmenta/util/__init__.py
@@ -0,0 +1,19 @@
+import nltk
+from nltk.corpus import stopwords
+
+
+def filter_stopwords(texto: str) -> str:
+    """
+    Filtra stopwords, removendo-as
+    :param texto: Texto a ser filtrado
+    :return: Texto filtrado
+    """
+    try:
+        nltk.data.find('corpora\stopwords')
+    except LookupError:
+        nltk.download('stopwords')
+    if len(texto) == 0:
+        return ""
+    p_stopwords = set(stopwords.words('portuguese'))
+    filtered = (w for w in texto.split() if w.lower() not in p_stopwords)
+    return " ".join(filtered)
