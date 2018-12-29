418a0e368678c3d9b57d5a9a25cfa6989e5011a6
Guilherme Scaranse <guiscaranse@gmail.com>
2018-12-29 12:59:56 -0300

    Adicionado comparador de artigos

---
+++ carmenta/controllers/ArticleComparator.py
@@ -0,0 +1,14 @@
+from typing import Tuple
+
+from fuzzywuzzy import fuzz
+
+from carmenta.util import filter_stopwords
+from jano.models import ArticleObject
+
+
+class ArticleComparator(object):
+    @staticmethod
+    def compare(a1: ArticleObject, a2: ArticleObject) -> Tuple[int, int]:
+        title = fuzz.token_sort_ratio(filter_stopwords(a1.titulo), filter_stopwords(a1.titulo))
+        corpus = fuzz.token_set_ratio(filter_stopwords(a1.texto), filter_stopwords(a2.texto))
+        return title, corpus
