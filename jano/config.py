5bee99e29d99c867e5ecc8670d84adea335b0421
Guilherme Scaranse <guiscaranse@gmail.com>
2018-12-30 22:37:06 -0300

    Adicionado cabeçalhos do dataset

--- jano/config.py
+++ jano/config.py
@@ -8,6 +8,8 @@ class Config(object):
             'language': "pt-BR",
             'replacement_charset': "latin1",
             'max_words_precision': 20,
-            'timezone': "America/Sao_Paulo"
+            'timezone': "America/Sao_Paulo",
+            'headers': ['title', 'corpus', 'ADJ', 'ADP', 'ADV', 'AUX', 'CONJ', 'DET', 'INTJ', 'NOUN', 'NUM', 'PART',
+                        'PRON', 'PROPN', 'PUNCT', 'SCONJ', 'SYM', 'VERB', 'X', 'length', 'good', 'bad', 'result']
         }
         return settings
