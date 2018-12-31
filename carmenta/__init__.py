3113eee9d49038b213c66f5f3ed4335514f35db1
Guilherme Scaranse <guiscaranse@gmail.com>
2018-12-30 23:54:17 -0300

    Adicionado length na saída

--- carmenta/__init__.py
+++ carmenta/__init__.py
@@ -1,6 +1,7 @@
 import jano
 from carmenta.controllers.EvaluateObjects import EvaluateObjects
 from carmenta.controllers.SemanticAnalyser import SemanticAnalyser
+from carmenta.util import count_words
 
 
 def score(url: str) -> dict:
@@ -13,4 +14,5 @@ def score(url: str) -> dict:
     evaluation = EvaluateObjects.evaluate(jano_data)
     data['comparators'] = evaluation
     data['semantic'] = {**SemanticAnalyser.gramatica(jano_data['original'].texto),**SemanticAnalyser.polaridade(jano_data['original'].texto)}
+    data['semantic']['length'] = count_words(jano_data['original'].texto)
     return data
