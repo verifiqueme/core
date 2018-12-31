10c287052c52fae731f94bc944694776efa86c02
Guilherme Scaranse <guiscaranse@gmail.com>
2018-12-30 21:13:12 -0300

    Corrigido comparação de Carmenta

--- carmenta/controllers/ArticleComparator.py
+++ carmenta/controllers/ArticleComparator.py
@@ -9,6 +9,6 @@ from jano.models import ArticleObject
 class ArticleComparator(object):
     @staticmethod
     def compare(a1: ArticleObject, a2: ArticleObject) -> Tuple[int, int]:
-        title = fuzz.token_sort_ratio(filter_stopwords(a1.titulo), filter_stopwords(a1.titulo))
+        title = fuzz.token_sort_ratio(filter_stopwords(a1.titulo), filter_stopwords(a2.titulo))
         corpus = fuzz.token_set_ratio(filter_stopwords(a1.texto), filter_stopwords(a2.texto))
         return title, corpus
