3c05b1b5db9a0595e0479a105188c33a0a8b09b9
Guilherme Scaranse <guiscaranse@gmail.com>
2018-12-30 23:54:17 -0300

    Adicionado length na saída

--- carmenta/util/__init__.py
+++ carmenta/util/__init__.py
@@ -1,5 +1,8 @@
 import nltk
 from nltk.corpus import stopwords
+from polyglot.text import Text
+
+from jano import Config
 
 
 def filter_stopwords(texto: str) -> str:
@@ -17,3 +20,8 @@ def filter_stopwords(texto: str) -> str:
     p_stopwords = set(stopwords.words('portuguese'))
     filtered = (w for w in texto.split() if w.lower() not in p_stopwords)
     return " ".join(filtered)
+
+
+def count_words(texto: str) -> int:
+    polyglot_text = Text(filter_stopwords(texto), hint_language_code=Config.values()['language'][:2])
+    return len(polyglot_text.words)
