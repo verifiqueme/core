4702e0a7988d9acebb217c5cc4ebdeb36a7c8ff9
Guilherme Scaranse <guiscaranse@gmail.com>
2019-01-01 17:16:22 -0300

    Instalado pyenv corretamente

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,6 +1,10 @@
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+export PATH="/app/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 pyenv install 3.6.7
+
 sudo apt-get install build-essential software-properties-common libicu-dev python-pyicu -y
-pip3 install pipenv
+pip install pipenv
 pipenv install --system
 find / -iname '*.cpp' -exec echo sed -i 's/locale\.h/locale2.h/g' '{}' ';'
