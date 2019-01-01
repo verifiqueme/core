4d4e6178234ab51b0641afe19b1b0937a2adc99e
Guilherme Scaranse <guiscaranse@gmail.com>
2019-01-01 17:13:08 -0300

    Adicionado mais etapas na construção do pacote

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,2 +1,6 @@
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+pyenv install 3.6.7
+sudo apt-get install build-essential software-properties-common libicu-dev python-pyicu -y
+pip3 install pipenv
 pipenv install --system
 find / -iname '*.cpp' -exec echo sed -i 's/locale\.h/locale2.h/g' '{}' ';'
