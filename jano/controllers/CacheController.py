bef0a4ffae4d5b08d1831d2e9cb5808b85c7e8c4
Guilherme Scaranse <guiscaranse@gmail.com>
2019-03-06 21:10:37 -0300

    É possível obter data do cache

--- jano/controllers/CacheController.py
+++ jano/controllers/CacheController.py
@@ -26,6 +26,16 @@ class CacheController(object):
             return False
 
     @staticmethod
+    def cacheAge(url: str):
+        hashed = hashlib.sha224(str(url).encode('utf-8')).hexdigest()
+        cache = os.path.normpath(jano_index() + os.path.normcase("/cache/{0}.jcache".format(hashed)))
+        if os.path.isfile(cache):
+            time = pendulum.from_timestamp(os.path.getmtime(cache))
+            return time
+        else:
+            return False
+
+    @staticmethod
     def createCache(dados: dict) -> bool:
         if 'original' not in dados and 'meta' not in dados:
             raise InvalidJunoObject("Dicionário deve ser um dicionário extraído de juno.extract_data")
