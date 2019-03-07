0dd1c5a460bb2b521fcfe15684462d90cf2dfc56
Guilherme Scaranse <guiscaranse@gmail.com>
2019-03-06 21:16:42 -0300

    Adicionado método para ajudar em construir datasets

--- pales/controllers/BuilderController.py
+++ pales/controllers/BuilderController.py
@@ -5,14 +5,8 @@ from jano import Config
 from pales.controllers.KerasController import KerasController
 
 
-def predict(url: str) -> list:
-    """
-    Prevê se uma notícia é verdadeira ou falsa (1 para verdadeiro, ou 0 para falsa)
-    :param url: a URL a ser analisada
-    :return: Retorna uma lista de probabilidade (1° coluna: chance de ser verdadeira, 2° coluna: chance de ser falsa)
-    """
+def translate_to_keras(url: str) -> list:
     info = dict()
-    keras = KerasController()
     for head in Config.values()['headers']:
         info[head] = 0
     info.pop('result', None)
@@ -21,5 +15,14 @@ def predict(url: str) -> list:
         info[key] = data['comparators'][key]
     for key in data['semantic'].keys():
         info[key] = data['semantic'][key]
-    universe = [[v for k, v in info.items()]]
-    return keras.keras_model().predict_proba(numpy.array(universe))[0]
+    return [[v for k, v in info.items()]]
+
+
+def predict(url: str) -> list:
+    """
+    Prevê se uma notícia é verdadeira ou falsa (1 para verdadeiro, ou 0 para falsa)
+    :param url: a URL a ser analisada
+    :return: Retorna uma lista de probabilidade (1° coluna: chance de ser verdadeira, 2° coluna: chance de ser falsa)
+    """
+    keras = KerasController()
+    return keras.keras_model().predict_proba(numpy.array(translate_to_keras(url)))[0]
