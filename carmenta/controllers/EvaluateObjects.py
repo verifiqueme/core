7ce95ca2e852076349ccb79f30b7de40c7cd5def
Guilherme Scaranse <guiscaranse@gmail.com>
2019-03-12 21:09:53 -0300

    Bloquear artigos de mais de 14 dias de diferença

--- carmenta/controllers/EvaluateObjects.py
+++ carmenta/controllers/EvaluateObjects.py
@@ -14,9 +14,10 @@ class EvaluateObjects(object):
             raise InvalidJunoObject("Dicionário deve ser um dicionário extraído de juno.extract_data")
         original: ArticleObject = dados['original']
         for relativo in dados['meta']:
-            comparador = ArticleComparator.compare(original, relativo)
-            resultados['title'] += comparador[0]
-            resultados['corpus'] += comparador[1]
+            if relativo.data is not None and relativo.data.diff(original.data).in_days() <= 14:
+                comparador = ArticleComparator.compare(original, relativo)
+                resultados['title'] += comparador[0]
+                resultados['corpus'] += comparador[1]
         resultados['title'] = round(resultados['title']/len(dados['meta']), 2) if resultados['title'] > 0 else 0
         resultados['corpus'] = round(resultados['corpus']/len(dados['meta']), 2) if resultados['corpus'] > 0 else 0
         return resultados
