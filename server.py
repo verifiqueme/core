97e9c259e6ae2d79a51e01e7a15de589264b2c94
Guilherme Scaranse <guiscaranse@gmail.com>
2019-03-19 17:58:47 -0300

    Adicionado opção para avaliar

--- server.py
+++ server.py
@@ -6,8 +6,10 @@ import re
 from abc import ABC
 from concurrent.futures import ThreadPoolExecutor
 
+import pendulum
 import tornado.ioloop
 import tornado.web
+from tinydb import TinyDB, Query
 from tornado import httpserver
 from tornado.concurrent import run_on_executor
 
@@ -17,6 +19,7 @@ from jano.models.ArticleObject import ArticleObject
 from pales.controllers.BuilderController import predict
 
 MAX_WORKERS = available_cpu_count()
+db = TinyDB('db.json')
 
 
 def decode_base64(data, altchars=b'+/'):
@@ -34,11 +37,59 @@ def decode_base64(data, altchars=b'+/'):
     return base64.b64decode(data, altchars).decode("utf-8")
 
 
-class APIHandler(tornado.web.RequestHandler, ABC):
+class BaseHandler(tornado.web.RequestHandler, ABC):
+    def set_default_headers(self, *args, **kwargs):
+        if os.environ.get('CORE_DEV'):
+            self.set_header("Access-Control-Allow-Origin", "*")
+            self.set_header("Access-Control-Allow-Headers", "x-requested-with, Origin, Content-Type, X-Auth-Token")
+
+
+class IndexHandler(BaseHandler, ABC):
+    async def hello(self):
+        return "hello"
+
+    async def get(self):
+        self.write(await self.hello())
+
+
+class RatingHandler(BaseHandler, ABC):
+    async def hello(self):
+        return "hello"
+
+    async def get(self):
+        self.write(await self.hello())
+
+    async def options(self):
+        self.write(await self.hello())
+
+    def post(self):
+        try:
+            Rating = Query()
+            url = str(self.get_argument('url'))
+            rating = int(self.get_argument('rating'))
+            table = db.table('rating')
+            search = table.search(Rating.url == url)
+            if search:
+                if rating == 1:
+                    table.update({'good': int(search[0]['good']) + rating}, Rating.url == url)
+                else:
+                    table.update({'bad': int(search[0]['bad'])}, Rating.url == url)
+            else:
+                table.insert({'url': url, 'good': 1 if int(rating) > 1 else 0, 'bad': 1 if int(rating) < 1 else 0})
+            data = "ok"
+            self.set_status(201)
+        except Exception as e:
+            self.set_status(401)
+            data = dict({"error": e.__str__()})
+            print(data)
+        self.write(json.dumps(data))
+
+
+class APIHandler(BaseHandler, ABC):
     executor = ThreadPoolExecutor(max_workers=MAX_WORKERS)
 
     @run_on_executor
-    def background_task(self, i):
+    def background_analyse(self, i):
         """ Isto sera executado em uma Pool. """
         result = predict(i)
         return str(result[0])
@@ -48,7 +99,7 @@ class APIHandler(tornado.web.RequestHandler, ABC):
         """ Chama a tarefa de fundo de forma assíncrona """
         try:
             url = decode_base64(query)
-            res = yield self.background_task(url)
+            res = yield self.background_analyse(url)
             metadados = extract_data(url)
             original: ArticleObject = metadados['original']
             words = 0
@@ -69,29 +120,25 @@ class APIHandler(tornado.web.RequestHandler, ABC):
                 },
                 "response": res
             }
+            table = db.table('history')
+            table.insert({'url': url, 'date': str(pendulum.now('America/Sao_Paulo'))})
+            self.set_status(200)
         except Exception as e:
             self.set_status(401)
             data = dict({"error": e.__str__()})
         self.write(json.dumps(data))
 
 
-class Teste(tornado.web.RequestHandler, ABC):
-    async def teste(self):
-        return "hello"
-
-    async def get(self):
-        self.write(await self.teste())
-
-
 def make_app():
     return tornado.web.Application([
-        (r"/", Teste),
+        (r"/", IndexHandler),
         (r"/api/([\s\S]*)", APIHandler),
+        (r"/rating", RatingHandler)
     ])
 
 
 if __name__ == "__main__":
-    print("Tornado it!")
+    print("Tornado it! Workers: {}".format(MAX_WORKERS))
     app = make_app()
     port = os.environ.get('PORT') if os.environ.get('PORT') else 8888
 
