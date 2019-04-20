541883f556c3f416f6e6b1cf673515945bf3c56b
Guilherme Scaranse <guiscaranse@gmail.com>
2019-04-20 01:28:55 -0300

    Corrigido código

--- extrair_dados_de_sites.py
+++ extrair_dados_de_sites.py
@@ -1,7 +1,9 @@
 import click
 import newspaper
 
-from pales.controllers.BuilderController import translate_to_keras
+from carmenta import score
+from jano import Config
+
 
 @click.command()
 @click.option("--url", prompt="Site a ser analisado",
@@ -13,8 +15,7 @@ def hello(url):
     for article in search.articles:
         try:
             print(article.url)
-            results = translate_to_keras(article.url)[0]
-            results.append(1)
+            results = translate_to_keras(article.url) + [1]
             with open("test.txt", "a") as myfile:
                 myfile.write(str(results) + "\n")
                 myfile.close()
@@ -22,8 +23,22 @@ def hello(url):
                 myfile.write(str(article.url) + "\n")
                 myfile.close()
             print("thank you next")
-        except Exception:
+        except Exception as e:
+            print(e.__str__())
             pass
+
+
+def translate_to_keras(url: str) -> list:
+    info = dict()
+    for head in Config.values()['headers']:
+        info[head] = 0
+    info.pop('result', None)
+    data = score(url)
+    for key in data['comparators'].keys():
+        info[key] = data['comparators'][key]
+    for key in data['semantic'].keys():
+        info[key] = data['semantic'][key]
+    return [v for k, v in info.items()]
 
 
 if __name__ == '__main__':
